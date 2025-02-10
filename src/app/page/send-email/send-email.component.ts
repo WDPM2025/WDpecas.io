c0f0ea6a21345b0d07a4b157c0d809379e7bbc10
WDPecasM <wdpecasmoto@gmail.com>
2025-02-10 19:59:31 +0000

    star the email send process...

--- src/app/page/send-email/send-email.component.ts
+++ src/app/page/send-email/send-email.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {EmailService} from '../../services/email.service'
 
 @Component({
   selector: 'app-send-email',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './send-email.component.css'
 })
 export class SendEmailComponent {
+
+  name: string;
+  email: string;
+  message: string;
+
+  constructor(private emailService: EmailService) {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
+
+  onSubmit(){
+    this.emailService.sendEmail(this.name, this.email, this.message).subscribe(
+      response => {
+        console.log('Email sent successfully!');
+      },
+      error => {
+        console.log('Error sending email:', error);
+      }
+    );
+
+  }
 
 }
